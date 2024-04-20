ece5cccacf1a67a6b46dcebb904112602db7edf6
NopeTornike <167585322+NopeTornike@users.noreply.github.com>
2024-04-20 04:17:08 +0400

    initial commit

---
+++ search/all_0.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['log_0',['Log',['../class_logger.html#a105a4681757ed9dddc377ef9cca7b2e2',1,'Logger']]],
+  ['logger_1',['Logger',['../class_logger.html',1,'']]],
+  ['logtofile_2',['LogToFile',['../class_logger.html#aeaee2a96ca4dcd858a1d1253a91249c2',1,'Logger']]]
+];
---
+++ search/functions_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['log_0',['Log',['../class_logger.html#a105a4681757ed9dddc377ef9cca7b2e2',1,'Logger']]],
+  ['logtofile_1',['LogToFile',['../class_logger.html#aeaee2a96ca4dcd858a1d1253a91249c2',1,'Logger']]]
+];
